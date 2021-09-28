cda1a98c9f7acd00fe4dcf991f190e74bef4bb38
Leoruiz197 <ruizruizleo@hotmail>
2021-09-27 22:49:06 -0300

    final aula node com EJS

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const express = require("express");
-const app = express();
+const express = require("express");  //importando o express
+const path = require("path");  // importando o path
+const app = express();  // definindo meu app como um express
+const port = 3000;  //set porta na 3000
 
-app.get("/", function (req, res){
-    res.send("hello bluemer");
+app.set("view engine", "ejs");  //set engine para trabalhar com o EJS
+
+app.use(express.static(path.join(__dirname, "public")));  //set public como pasta raiz de arquivos estaticos como imgs e css
+
+app.get("/", function (req, res){ // .get para tratar as requisicoes tipo get e a rota solicitada
+    res.render("index", { titulo: "ola bluemer 123" });  // render para renderizar o arquivo EJS dentro da views por padrao
+});
+
+app.get("/pagina", function (req, res){
+    res.render("pagina", { titulo: "pagina 2 123" });
 });
 
 app.get("/teste", function (req, res){
-    res.send("esse é um teste");
-});
-
-app.get("/teste2", function (req, res){
-    res.send("esse é outro teste");
+    res.render("../views/ejs/pagina2", { titulo: "pagina modificada" })
 });
 
 // app.post
 // app.put
 // app.delete
-
-app.listen(3000);
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`)); // faz o servidor escutar, passando a porta onde o servico vai ficar ativo
