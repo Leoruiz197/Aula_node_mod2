514a180978727cea38cf403ca74d1a2f119b1b79
Leoruiz197 <ruizruizleo@hotmail>
2021-10-01 17:58:21 -0300

    teste

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");  //importando o express
 const path = require("path");  // importando o path
 const app = express();  // definindo meu app como um express
-const port = 3000;  //set porta na 3000
+const port = process.env.PORT || 3000; //set porta na variavel de ambiente ou na 3000
 
 app.set("view engine", "ejs");  //set engine para trabalhar com o EJS
 
